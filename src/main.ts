925e9b8f0548d2e46c542ce9e4a401060238bd3c
LuisFillype <l.fillype@gmail.com>
2022-04-05 12:35:58 -0300

    feat: add swager configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,47 @@
+import { NestApplicationOptions } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import fastifyHelmet from 'fastify-helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+
+  app.register(fastifyHelmet, {
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: [`'self'`],
+        styleSrc: [`'self'`, `'unsafe-inline'`],
+        imgSrc: [`'self'`, 'data:', 'validator.swagger.io'],
+        scriptSrc: [`'self'`, `https: 'unsafe-inline'`],
+      },
+    },
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('NestJs-Softwares')
+    .setDescription('This is a API documentation for nestjs softwares project.')
+    .setVersion('1.0')
+    .addTag('NestJs-Softwares v1')
+    .addBearerAuth({
+      type: 'http',
+      description:
+        'Example: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000, () => {
+    console.log('Server is running at: http://localhost:3000');
+  });
 }
 bootstrap();
