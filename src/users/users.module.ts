f93c8b1d5e8b03005de91ae400b58c7fa968f54e
LuisFillype <l.fillype@gmail.com>
2022-04-05 19:45:58 -0300

    feat: add provider on module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import BCryptHashProvider from 'src/providers/hashProvider';
+import JWTProvider from 'src/providers/jwtProvider';
 import { User } from './entities/user.entity';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
@@ -8,6 +9,6 @@ import { UsersService } from './users.service';
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
-  providers: [UsersService, BCryptHashProvider],
+  providers: [UsersService, BCryptHashProvider, JWTProvider],
 })
 export class UsersModule {}
