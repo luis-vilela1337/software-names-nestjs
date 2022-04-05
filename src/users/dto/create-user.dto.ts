6522e322e75abb84cbbf08e04c0fb09bf0688840
LuisFillype <l.fillype@gmail.com>
2022-04-05 12:56:57 -0300

    feat: create users DTO

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,66 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsEnum,
+  IsNotEmpty,
+  IsString,
+} from 'class-validator';
+import { UserFunction } from '../entities/users.entety';
+
+export class CreateUserDto {
+  /**
+   * @example newUser
+   */
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  /**
+   * @example user@email.com
+   */
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  /**
+   * @example 12345
+   */
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+
+  /**
+   * @example 55999999999
+   */
+  @IsString()
+  @IsNotEmpty()
+  phone: string;
+
+  /**
+   * @example 55999999999
+   */
+  @IsString()
+  @IsNotEmpty()
+  whatsapp: string;
+
+  /**
+   * @example Brasília
+   */
+  @IsString()
+  @IsNotEmpty()
+  city: string;
+
+  /**
+   * @example true
+   */
+  @IsBoolean()
+  @IsNotEmpty()
+  status: boolean;
+
+  /**
+   * @example user
+   */
+  @IsEnum(UserFunction)
+  @IsNotEmpty()
+  function: UserFunction;
+}
