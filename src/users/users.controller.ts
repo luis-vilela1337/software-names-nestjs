a48202b2ca83badb888cdeca62739dfd75096565
LuisFillype <l.fillype@gmail.com>
2022-04-05 19:45:42 -0300

    feat: add middleware of auth

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,9 +6,11 @@ import {
   Param,
   Post,
   Req,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { ApiBearerAuth, ApiParam, ApiTags } from '@nestjs/swagger';
+import { AuthMiddleware } from 'src/middleware/auth';
 
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
@@ -20,6 +22,7 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @ApiBearerAuth()
+  @UseGuards(AuthMiddleware)
   @Post()
   create(@Req() request: any, @Body() createUserDto: CreateUserDto) {
     const loggedUser = request.user.id;
@@ -32,6 +35,7 @@ export class UsersController {
     description: 'uuid',
     example: '84985d78-6510-4586-ae48-3c234026d1dd',
   })
+  @UseGuards(AuthMiddleware)
   @Get(':id')
   findOne(@Req() request: any, @Param('id') id: string) {
     const loggedUser = request.user.id;
@@ -40,6 +44,7 @@ export class UsersController {
 
   @ApiBearerAuth()
   @Get()
+  @UseGuards(AuthMiddleware)
   findAll(@Req() request: any) {
     const loggedUser = request.user.id;
     return this.usersService.findAll(loggedUser);
