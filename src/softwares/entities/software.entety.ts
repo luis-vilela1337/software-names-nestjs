dbb3250870953b61f5ddff670c0f68d20656e176
LuisFillype <l.fillype@gmail.com>
2022-04-06 14:40:29 -0300

    feat: create entety for user

---
+++ src/softwares/entities/software.entety.ts
@@ -0,0 +1,48 @@
+import { User } from 'src/users/entities/user.entity';
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ name: 'softwares' })
+export class Software extends BaseEntity {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  link: string;
+
+  @Column('text', { array: true })
+  tags: string[];
+
+  @ManyToOne(() => User, (user) => user.id, {
+    eager: true,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn({
+    default: () => 'NOW()',
+  })
+  created_at: Date;
+
+  @UpdateDateColumn({
+    default: () => 'NOW()',
+    onUpdate: 'NOW()',
+  })
+  updated_at: Date;
+}
